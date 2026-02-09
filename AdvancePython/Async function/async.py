48a062d3c51155a27a625e25621209f034b42a29
Pareexit-Baldaniya-Innvonix <pareexit.baldaniya@innvonix.in>
2026-02-09 17:10:37 +0530

    small modification using comments

--- AdvancePython/Async function/async.py
+++ AdvancePython/Async function/async.py
@@ -4,14 +4,14 @@ import asyncio
 # task 1
 async def task1() -> None:
     print("Task 1 started")
-    await asyncio.sleep(4)
+    await asyncio.sleep(4)  # taking 4 sec. to complete
     print("Task 1 completed")
 
 
 # task 2
 async def task2() -> None:
     print("Task 2 started")
-    await asyncio.sleep(1)
+    await asyncio.sleep(1)  # taking 1 sec. to complete
     print("Task 2 completed")
 
 
